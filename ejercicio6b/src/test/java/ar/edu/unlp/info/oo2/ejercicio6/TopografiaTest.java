0f2f4235231d734eb4d7369d97723b57259de8f7
CHAVERO BOCEK FABRICIO LEONEL <fabochavero@gmail.com>
2024-04-21 22:57:26 -0300

    agregando tests ejercicio6b

--- ejercicio6b/src/test/java/ar/edu/unlp/info/oo2/ejercicio6/TopografiaTest.java
+++ ejercicio6b/src/test/java/ar/edu/unlp/info/oo2/ejercicio6/TopografiaTest.java
@@ -13,8 +13,8 @@ import org.junit.jupiter.api.Test;
 public class TopografiaTest {
 
 	// declaras
-	private Topografia agua,tierra,mixta1, mixta2, mixta3, mixta4;
-	private Simple water, water2,dirt;
+	private Topografia agua,tierra,mixta1, mixta2, mixta3, mixta4,pantano,mixta5,mixta6;
+	private Simple water, water2,dirt,swamp;
 	
 	
 	// inicializas
@@ -26,9 +26,14 @@ public class TopografiaTest {
 		mixta2 = new Mixta();
 		mixta3 = new Mixta();
 		mixta4 = new Mixta();
+		mixta5 = new Mixta();
+		mixta6 = new Mixta();
+		pantano =new Mixta();
+		
 		water = new Agua();
 		water2 = new Agua();
 		dirt = new Tierra();
+		swamp = new Pantano();
 		
 		agua.agregar(water);
 		agua.agregar(water);
@@ -40,6 +45,11 @@ public class TopografiaTest {
 		tierra.agregar(dirt);
 		tierra.agregar(dirt);
 		
+		pantano.agregar(swamp);
+		pantano.agregar(swamp);
+		pantano.agregar(swamp);
+		pantano.agregar(swamp);
+
 		mixta1.agregar(water);
 		mixta1.agregar(dirt);
 		mixta1.agregar(water);
@@ -59,6 +69,17 @@ public class TopografiaTest {
 		mixta4.agregar(dirt);
 		mixta4.agregar(dirt);
 		mixta4.agregar(mixta1);
+		
+		mixta5.agregar(water);
+		mixta5.agregar(dirt);
+		mixta5.agregar(dirt);
+		mixta5.agregar(swamp);
+		
+		mixta6.agregar(water);
+		mixta6.agregar(dirt);
+		mixta6.agregar(dirt);
+		mixta6.agregar(swamp);
+		
 	}
 	
 	// testeas
@@ -69,6 +90,9 @@ public class TopografiaTest {
 		assertEquals(0.5, mixta1.calcular());
 		assertEquals(0.375, mixta2.calcular());
 		assertEquals(1, water.calcular());
+		
+		assertEquals(0.7,pantano.calcular());
+		assertEquals(0.425,mixta5.calcular());
 	}
 	
 	@Test
@@ -79,6 +103,9 @@ public class TopografiaTest {
 		assertTrue(water.comparar(water2));
 		assertFalse(mixta2.comparar(mixta3));
 		assertTrue(mixta4.comparar(mixta2));
+		
+		assertTrue(mixta5.comparar(mixta6));
+		
 	}
 	
 	
